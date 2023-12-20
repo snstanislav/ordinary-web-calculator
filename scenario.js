7e51b5de66ec1e72bc0a43455cd3d0422ba0f4fe
Stanislav Snisar <sn.stanislav4@gmail.com>
2023-12-20 19:27:34 +0200

    Initial commit for the project. Include markup, styles and blank js file

---
+++ scenario.js
@@ -0,0 +1,9 @@
+
+function init(document) {
+let four = document.getElementById('multiple').innerHTML;
+
+let select = document.querySelectorAll(".buttonsRow > div")
+console.log(select.entries())
+
+
+}
