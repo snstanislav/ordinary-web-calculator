cfe03bb697977f47fd2bff30577d3173373fadc4
Stanislav Snisar <sn.stanislav4@gmail.com>
2024-01-07 22:16:07 +0200

    Refactoring, temporary changes in scenario.js and other files

--- scenario.js
+++ scenario.js
@@ -1,16 +1,17 @@
-let firstBinaryOperand = undefined;
-let secondBinaryOperand = undefined;
+let frstBinOperand = undefined;
+let scndBinOperand = undefined;
 let binaryOperation = undefined;
+let currCalcResult = undefined;
 let currScreenValue = undefined;
-let currentResult = 0;
 let isFloatPointOn = false;
-let isFrstInput = false;
-let isScndInput = false;
+
+/* Temporary log container */
+let stateDisplay;
 
 let addingOperation = (first, second) => Number(first)+Number(second);
-let subtractOperation = (first, second) => first-second;
-let multipleOperation = (first, second) => first*second;
-let divideOperation = (first, second) => first/second;
+let subtractOperation = (first, second) => Number(first)-Number(second);
+let multipleOperation = (first, second) => Number(first)*Number(second);
+let divideOperation = (first, second) => Number(first)/Number(second);
 let sqrootOperation = value => Math.sqrt(value);
 let percentOperation = value => value*0.01;
 
@@ -19,124 +20,136 @@ function init(document) {
 
   let buttonsCollection = document.querySelectorAll(".buttonsRow > div");
 
-  isFrstInput = true;
-  isScndInput = false;
+  ///
+  stateDisplay = document.querySelector("body > div > p");
 
   for (let elem of buttonsCollection) {
-    elem.addEventListener("click", perform);
+    elem.addEventListener("click", performClick);
   }
+
+  ///
+  snapshot("after init");
 }
 
-function perform(event) {
+function performClick(event) {
   let buttonElement = event.target;
   if (buttonElement.className == "digit") {
-    if (isFrstInput && !isScndInput) {
-      increaseScreen(buttonElement.innerText);
-    }
-    if (!isFrstInput && !isScndInput) {
-      showOnScreen(buttonElement.innerText);
-      isScndInput = true;
-    } else if (!isFrstInput && isScndInput) {
-      increaseScreen(buttonElement.innerText);
-    }
-
-    if (getFromScreen().charAt(0) == "0" && getFromScreen().length > 1 && getFromScreen().charAt(1) != ".") {
-      showOnScreen(
-        getFromScreen().substring(1));
-    }
+    applyDigit(buttonElement.innerText);
   }
   switch (buttonElement.id) {
     case "reset":
-      showOnScreen("0");
-      firstBinaryOperand = undefined;
-      secondBinaryOperand = undefined;
-      binaryOperation = undefined;
-      isFrstInput = true;
-      isScndInput = false;
-
-      snapshot("===afret reset===")
+      applyReset();
+      ///
+      snapshot("case: afret reset");
       break;
     case "backspace":
-      if (isFrstInput || isScndInput) {
-        showOnScreen(getFromScreen().substring(0, getFromScreen().length-1));
-        if (!getFromScreen().includes(".")) {
-          isFloatPointOn = false;
-        }
-        if (getFromScreen().length < 1) {
-          showOnScreen("0");
-        }
-      }
+      applyBackspace();
+      ///
+      snapshot("case: after backspace");
       break;
     case "flpoint":
-      if (isFloatPointOn == false) {
-        increaseScreen(buttonElement.innerText);
-        isFloatPointOn = true;
-      }
+      applyFloatPoint(buttonElement.innerText);
+      ///
+      snapshot("case: after float point");
       break;
     case "sqroot":
-      showOnScreen(sqrootOperation(getFromScreen()));
+      setScreenValue(sqrootOperation(getFromScreen()));
+      ///
+      snapshot("case: after square root");
       break;
     case "percent":
-      showOnScreen(percentOperation(getFromScreen()));
+      setScreenValue(percentOperation(getFromScreen()));
+      ///
+      snapshot("case: after percent");
       break;
     case "equals":
-      if (firstBinaryOperand != undefined) {
+      if (frstBinOperand != undefined && binaryOperation != undefined) {
         processBinary(binaryOperation);
 
+        ///
+        snapshot("case: after equals");
       } else {
         snapshot("Equals will not perform");
       }
       break;
     case "divide":
+      ///
+      snapshot("case: before divide");
       processBinary(divideOperation);
+      ///
+      //snapshot("case: after divide");
       break;
     case "multiple":
+      ///
+      snapshot("case: before multip");
       processBinary(multipleOperation);
+      ///
+     // snapshot("case: after multip");
       break;
     case "subtract":
+      ///
+      snapshot("case: before subtra");
       processBinary(subtractOperation);
+      ///
+     // snapshot("case: after substra");
       break;
     case "add":
+      ///
+      snapshot("case: before add");
       processBinary(addingOperation);
+      ///
+     // snapshot("case: after add");
       break;
   }
 }
 
 function processBinary(currentOperation) {
   binaryOperation = currentOperation;
-  isFrstInput = false;
-  
-  if (firstBinaryOperand == undefined && secondBinaryOperand == undefined) {
-    firstBinaryOperand = getFromScreen();
-    snapshot("===processBinary first stage===");
+  if (frstBinOperand == undefined && scndBinOperand == undefined) {
+    
+    frstBinOperand = getFromScreen();
+    snapshot("processBinary - first stage");
+    currCalcResult = undefined;
+  } else if (frstBinOperand != undefined && scndBinOperand == undefined) {
+    scndBinOperand = getFromScreen();
 
-  } else if (firstBinaryOperand != undefined && secondBinaryOperand == undefined) {
-    secondBinaryOperand = getFromScreen();
-
-    showOnScreen(equalsOperation(firstBinaryOperand, secondBinaryOperand,
+   setScreenValue(equalsOperation(frstBinOperand, scndBinOperand,
       binaryOperation));
-    isScndInput = false;
-    snapshot("===processBinary second stage===");
+      /*
+setScreenValue(currentOperation(frstBinOperand, scndBinOperand));
+*/
 
-  } else if (firstBinaryOperand != undefined && secondBinaryOperand != undefined) {
-     isFrstInput = false;
-      isScndInput = false;
+    snapshot("processBinary - second stage");
 
-    showOnScreen(equalsOperation(firstBinaryOperand, secondBinaryOperand,
+  } else if (frstBinOperand != undefined && scndBinOperand != undefined &&
+  binaryOperation != undefined) {
+
+    currCalcResult = undefined;
+
+    setScreenValue(equalsOperation(frstBinOperand, scndBinOperand,
       binaryOperation));
 
-    snapshot("===processBinary third stage===");
+    /*setScreenValue(currentOperation(frstBinOperand, scndBinOperand));*/
+/*
+     frstBinOperand = scndBinOperand;
+      scndBinOperand = undefined;
+binaryOperation = undefined;*/
+
+    snapshot("processBinary - third stage");
 
   }
 }
 
 function equalsOperation(tempFrstOp, tempScndOp, tempBinOp) {
+  currCalcResult = undefined;
+
   snapshot("===equalsOperation called===");
 
   if (tempFrstOp != undefined && tempScndOp != undefined && tempBinOp != undefined) {
 
     const result = tempBinOp(tempFrstOp, tempScndOp);
-    firstBinaryOperand = result;
+    frstBinOperand = result;
+    // scndBinOperand = undefined;
     return result;
   }
 }
@@ -144,17 +157,83 @@ function equalsOperation(tempFrstOp, tempScndOp, tempBinOp) {
 function getFromScreen() {
   return currScreenValue.innerText;
 }
-function showOnScreen(value) {
+function setScreenValue(value) {
   currScreenValue.innerText = value;
 }
-function increaseScreen(value) {
+function appendScreenValue(value) {
   currScreenValue.innerText += value;
 }
 
+function applyReset() {
+  setScreenValue("0");
+  frstBinOperand = undefined;
+  scndBinOperand = undefined;
+  binaryOperation = undefined;
+  currCalcResult = undefined;
+}
+
+function applyDigit(buttValue) {
+  if ((frstBinOperand == undefined && scndBinOperand == undefined) ||
+    frstBinOperand != undefined && binaryOperation != undefined /*&&
+      scndBinOperand == undefined*/) {
+
+    if (currCalcResult == undefined) {
+      setScreenValue(buttValue);
+      currCalcResult = NaN;
+    } else
+      appendScreenValue(buttValue);
+  }
+
+  if (getFromScreen().charAt(0) == "0" && getFromScreen().length > 1 && getFromScreen().charAt(1) != ".") {
+    setScreenValue(
+      getFromScreen().substring(1));
+  }
+}
+
+function applyBackspace() {
+  if (!getFromScreen().includes(".")) {
+    isFloatPointOn = false;
+  }
+  if ((frstBinOperand == undefined && scndBinOperand == undefined) ||
+    frstBinOperand != undefined && binaryOperation != undefined /*&&
+        scndBinOperand == undefined*/) {
+    if (currCalcResult != undefined) {
+      setScreenValue(getFromScreen().substring(0, getFromScreen().length-1));
+
+      if (getFromScreen().length < 1) {
+        setScreenValue("0");
+      }
+    }
+  }
+}
+
+function applyFloatPoint(buttValue) {
+  if (!getFromScreen().includes(".")) {
+    isFloatPointOn = false;
+  }
+  if ((frstBinOperand == undefined && scndBinOperand == undefined) ||
+    frstBinOperand != undefined && binaryOperation != undefined /*&&
+        scndBinOperand == undefined*/) {
+    if (currCalcResult != undefined &&
+      isFloatPointOn == false) {
+      appendScreenValue(buttValue);
+      isFloatPointOn = true;
+    }
+  }
+}
+
 function snapshot(place) {
-  console.log(place + "\n" + getFromScreen() + "\nfirstBinaryOperand " +
-    firstBinaryOperand + "\nsecondBinaryOperand " + secondBinaryOperand +
-    "\nbinaryOperation " + binaryOperation + "\nisFrstInput " + isFrstInput +
-    "\nisScndInput " + isScndInput
-    + "\n----------------");
+  stateDisplay.innerText = 
+    "source --- " + place + 
+    "\nonScreen -- " + getFromScreen() + "\nfirst bin. operand -- " +
+    frstBinOperand + 
+    "\nsecond bin. operand -- " + scndBinOperand +
+    "\nbinary operation :: " + binaryOperation + "\n\ncurrCalcResult -- " + currCalcResult +
+    "\nisFloatPointOn -- " + isFloatPointOn;
+
+  console.log("source -- " + place + "\nonScreen -- " + getFromScreen() + "\nfirst bin. operand -- " +
+    frstBinOperand + 
+    "\nsecond bin. operand -- " + scndBinOperand +
+    "\nbinary operation -- " + binaryOperation + "\n\ncurrCalcResult -- " + currCalcResult +
+    "\nisFloatPointOn -- " + isFloatPointOn);
 }
