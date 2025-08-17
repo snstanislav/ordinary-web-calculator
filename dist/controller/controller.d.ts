a744dd4201ce4b490e26fefce49b5cff9314daaf
Stanislav Snisar <sn.stanislav4@gmail.com>
2025-08-17 18:46:04 +0200

    Remove dist/

--- dist/controller/controller.d.ts
+++
@@ -1,3 +0,0 @@
-import { type OutputSet } from "../settings.js";
-export declare function getController(output: OutputSet): (event: MouseEvent) => void;
-//# sourceMappingURL=controller.d.ts.map
--- dist/model/calculation.d.ts
+++
@@ -1,3 +0,0 @@
-import { type SingleCalculation } from "../settings.js";
-export declare function performCalc(calc: SingleCalculation): void;
-//# sourceMappingURL=calculation.d.ts.map
